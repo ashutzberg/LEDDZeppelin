a3cd22d8b6f72ab45241fce9c5ad20095e3a7d24
kmcclelland3 <kristy97@comcast.net>
2018-09-11 12:38:26 -0400

    considering different types of image classifiers

--- classifier.m
+++ classifier.m
@@ -1,8 +1,13 @@
 % parse data from folder
 % file name indicates label for that image
 
-% SVM classifier on images
+% extract features
 
+% SVM classifier on images
+% X is the image data/features and y is the labels
+clf = fitcsvm(X,y)
+% imgs is the image data/features and bag is the labels
+clf = trainImageCategoryClassifier(imgs, bag)
 % train
 
 % test

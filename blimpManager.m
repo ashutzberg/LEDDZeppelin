8d13274f2d8221ff59ca04a64bb792a7042a2d8c
kmcclelland3 <kmcclelland3@gatech.edu>
2018-10-24 23:15:04 -0400

    working on windowing filter for classifier

--- blimpManager.m
+++ blimpManager.m
@@ -1,26 +1,40 @@
 function [] = blimpManager()
 
 % call speech recognition code and decipher the user's destination
+target_destination = 1
 
 % initialize Ros
 [tfSub, imageSub] = initializeRos();
 
 % call classifier
-while (true)
-   [tagX, tagY, tagZ, tagYaw, tagLabel] = getTagPose(tfSub)
-   pause(2)
-end
+
+[tagX, tagY, tagZ, tagYaw, tagLabels] = getTagPose(tfSub)
+pause(2)
 
 % if no tags within vicinity
+while labLabels(1) == -1 && tagLabels(2) == -1
+    % turn blimp in increasing spiral to increase chance of detecting tag
+    [tagX, tagY, tagZ, tagYaw, tagLabels] = getTagPose(tfSub)
+end
 
-% if only incorrect tag is in camera frame
 % call localization script
-% based on global position, correct to point camera at destination tag
+% work with eric to determine what info should be passed in
+% which tag to pass in if two tags were detected
+% take average of x,y,z,yaw?
+[x,y,thetaglobal] = blimpLocalization(xTagtoBlimp,yTagtoBlimp,thetaTagtoBlimp,sigmaSensor);
 
 % if correct tag is within camera frame
-% call localization script
+if tagLables(1) == target_destination
+    
+elseif tagLables(2) == target_destination
+    
+% if only incorrect tag is in camera frame
+% based on global position, correct to point camera at destination tag
+else
+        
+end
 
-% call controls
+% call controls (potentially grab image?)
 % controls calls classifier to correct navigation
 
 % shutdown ros
--- getTagPose.m
+++ getTagPose.m
@@ -1,34 +1,83 @@
-function [tagX, tagY, tagZ, tagYaw, tagLabel] = getTagPose(tfSub)
+function [tagX, tagY, tagZ, tagYaw, tagLabels] = getTagPose(tfSub)
 
 % poll for so many images
+WINDOW_SIZE = 12;
 % determine if more than one tag detected
 % for each tag determine if more than 4? other tag detections agree
 
-message_buffer = zeros(1,20)
-for i = 1:20
+message_buffer = robotics.ros.msggen.tf2_msgs.TFMessage.empty(WINDOW_SIZE, 0);
+for i = 1:WINDOW_SIZE
+    % handle error if receive errors out
    message_buffer(i) = receive(tfSub, 10);
 end
-tags = zeros(1,8)
-for i = 1:20
-    msg = message_buffer(i)
-    tag = msg.Transforms.ChildFrameId
-    tag_num = regexp(tag, '\d*', 'Match');
-    
-    quat = msg.Transforms.Transform.Rotation;
-    [pitch,yaw,roll] = quat2angle([quat.W quat.X quat.Y quat.Z], 'XYZ');
-    x = msg.Transforms.Transform.Translation.X;
-    y = msg.Transforms.Transform.Translation.Y;
-    z = msg.Transforms.Transform.Translation.Z;
-    
+
+BUFFER_SIZE = 8;
+TAG_COUNT = 2;
+tags = zeros(1,BUFFER_SIZE);
+tag_labels = [-1, -1];
+first_tag_x = zeros(1,BUFFER_SIZE);
+first_tag_y = zeros(1,BUFFER_SIZE);
+first_tag_z = zeros(1,BUFFER_SIZE);
+first_tag_yaw = zeros(1,BUFFER_SIZE);
+
+second_tag_x = zeros(1,BUFFER_SIZE);
+second_tag_y = zeros(1,BUFFER_SIZE);
+second_tag_z = zeros(1,BUFFER_SIZE);
+second_tag_yaw = zeros(1,BUFFER_SIZE);
+
+% implement windowing
+for i = 1:WINDOW_SIZE
+    msg = message_buffer(i);
+    tag = msg.Transforms.ChildFrameId;
+    tag_num = cell2mat(regexp(tag, '\d*', 'Match'))
+    if tag_num <= 7 && tag_num >= 0
+        continue
+    end
+    tags(tag_num) = tags(tag_num) + 1;
 end
-msg = receive(tfSub, 10); % 10 s timeout until message
-quat = msg.Transforms.Transform.Rotation;
-rotm = quat2rotm([quat.W quat.X quat.Y quat.Z]);
-%[roll,pitch,yaw] = quat2angle([quat.W quat.X quat.Y quat.Z], 'XYZ')
-[pitch,yaw,roll] = quat2angle([quat.W quat.X quat.Y quat.Z], 'XYZ');
-x = msg.Transforms.Transform.Translation.X
-y = msg.Transforms.Transform.Translation.Y
-z = msg.Transforms.Transform.Translation.Z
-tag_num = msg.Transforms.ChildFrameId
 
+first_i = 1;
+second_i = 1;
+for i = 1:WINDOW_SIZE
+    msg = message_buffer(i);
+    tag = msg.Transforms.ChildFrameId;
+    tag_num = cell2mat(regexp(tag, '\d*', 'Match'))
+    % throw out erroneous detections by making sure that there have been
+    % atleast 4 detections of the same tag within the last WINDOW_SIZE
+    % messages
+    if tags(tag_num) >= 4
+        if first_i == 1
+            tag_labels(1) = tag_num
+        else
+            tag_labels(2) = tag_num
+        end
+        quat = msg.Transforms.Transform.Rotation;
+        [~,yaw,~] = quat2angle([quat.W quat.X quat.Y quat.Z], 'XYZ');
+        x = msg.Transforms.Transform.Translation.X;
+        y = msg.Transforms.Transform.Translation.Y;
+        z = msg.Transforms.Transform.Translation.Z;
+        if tag_num == tag_labels(1)
+            first_tag_x(first_i) = x;
+            first_tag_y(first_i) = y;
+            first_tag_z(first_i) = z;
+            first_tag_yaw(first_i) = yaw;
+            first_i = first_i + 1;
+        else
+            second_tag_x(second_i) = x;
+            second_tag_y(second_i) = y;
+            second_tag_z(second_i) = z;
+            second_tag_yaw(second_i) = yaw;
+            second_i = second_i + 1;
+        end
+    end
+end
 
+% do i need to verify the reasonableness of the x,y,z, and yaw coordinates?
+
+% x,y,z are in meters
+% yaw is in degrees?
+tagX = [first_tag_x; second_tag_x]
+tagY = [first_tag_y; second_tag_y]
+tagZ = [first_tag_z, second_tag_z]
+tagYaw = [first_tag_yaw; second_tag_yaw]
+tagLabels = tag_labels
